fd7d0f60f2536e973f96b634f360f2eaa1d23f0e
soyeon <wsy4533@naver.com>
2021-08-27 17:34:27 +0900

    calculator

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-const a = 5;
-const b = 2;
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  multi: function (a, b) {
+    console.log(a * b);
+  },
+  divide: function (a, b) {
+    console.log(a / b);
+  },
+  powerOf: function (a, b) {
+    console.log(a ** b);
+  },
+};
 
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
+calculator.add(30, 5);
+calculator.minus(30, 5);
+calculator.multi(30, 5);
+calculator.divide(30, 5);
+calculator.powerOf(2, 3);
